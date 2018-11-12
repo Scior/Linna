eb8b0d282e940fcbca275f8c1e68003918bc45b3
Scior <interval12@gmail.com>
2018-11-12 21:34:45 +0900

    Initial Commit

---
+++ Linna/Linna.h
@@ -0,0 +1,19 @@
+//
+//  Linna.h
+//  Linna
+//
+//  Created by Suita Fujino on 2018/11/12.
+//  Copyright © 2018 Suita Fujino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Linna.
+FOUNDATION_EXPORT double LinnaVersionNumber;
+
+//! Project version string for Linna.
+FOUNDATION_EXPORT const unsigned char LinnaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Linna/PublicHeader.h>
+
+
