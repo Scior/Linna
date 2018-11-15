2fe2a2375143574e097dc914d2eaedd06e8b33ee
Scior <interval12@gmail.com>
2018-11-15 20:36:34 +0900

    Add CustomizableLogFormatter with tests

---
+++ Sources/Formatter/CustomizableLogFormatter.swift
@@ -0,0 +1,35 @@
+//
+//  CustomizableLogFormatter.swift
+//  Linna
+//
+//  Created by Suita Fujino on 2018/11/15.
+//  Copyright © 2018 Suita Fujino. All rights reserved.
+//
+
+/**
+ Log formatter with a customizable format.
+ */
+class CustomizableLogFormatter: LogFormatter {
+
+    // MARK: - Properties
+    
+    private let pattern: String
+    
+    // MARK: - Lifecycle
+    
+    /**
+     Initialize with the log format pattern.
+     
+     - Parameters:
+       - pattern: Log format pattern. See `LogFormatter.format`.
+     */
+    init(pattern: String) {
+        self.pattern = pattern
+    }
+    
+    // MARK: - Methods
+    
+    func format(from contents: LogContents) -> String? {
+        return format(from: contents, with: pattern)
+    }
+}
