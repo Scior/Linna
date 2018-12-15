fe4354acaae6c0b6a796e57443c7f81918a407ee
Scior <interval12@gmail.com>
2018-12-15 15:41:09 +0900

    Fix and add some test cases

--- Tests/Formatter/LogFormatterTests.swift
+++ Tests/Formatter/LogFormatterTests.swift
@@ -12,17 +12,30 @@ import XCTest
 class LogFormatterTests: XCTestCase {
     
     private let formatter = DefaultLogFormatter()
-    private let contents = LogContents(
+    private let contents1 = LogContents(
         date: "1234-11-22 xx:xx:xx",
         level: .info,
         objects: ["XAXA", "BBXX"],
         caller: Caller(filePath: "HogeFile", functionName: "HogeFunc", lineNumber: 3_333)
     )
+    private let contents2 = LogContents(
+        date: "2018-01-01 00:00:00",
+        level: .error,
+        objects: ["Fuga"],
+        caller: Caller(filePath: "FugaFile", functionName: "FugaFunc", lineNumber: 12)
+    )
 
-    func testFormatWithPattern() {
+    func testFormatWithPattern1() {
         XCTAssertEqual(
-            formatter.format(from: contents, with: "%d %obj <%level> #%file:%func:%line#"),
-            "\(contents.date) XAXA BBXX <\(contents.level.outputName())> #\(contents.caller.filePath):\(contents.caller.functionName):\(contents.caller.lineNumber)#")
+            formatter.format(from: contents1, with: "%d %obj <%level> #%file:%func:%line#"),
+            "\(contents1.date) XAXA BBXX <\(contents1.level.outputName())> #\(contents1.caller.filePath):\(contents1.caller.functionName):\(contents1.caller.lineNumber)#")
+        
+    }
+    
+    func testFormatWithPattern2() {
+        XCTAssertEqual(
+            formatter.format(from: contents2, with: "%d %obj [%level] #%file::%func:%line#"),
+            "\(contents2.date) Fuga [\(contents2.level.outputName())] #\(contents2.caller.filePath)::\(contents2.caller.functionName):\(contents2.caller.lineNumber)#")
         
     }
 
